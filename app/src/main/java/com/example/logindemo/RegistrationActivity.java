7df1b6f1d4da271b18862042fb09708ba92d18ff
Neha Pendem <nehapendem6@gmail.com>
2020-07-14 19:57:04 +0530

    First Commit

---
+++ app/src/main/java/com/example/logindemo/RegistrationActivity.java
@@ -0,0 +1,158 @@
+package com.example.logindemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String TAG = "tag";
+    private EditText userName, userPassword, userEmail,userAge;
+
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+    private ImageView userProfilePic;
+    String email,name,age,password;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        setupUIViews();
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("enter","eneterd button");
+
+                if(validate()){
+                    Log.d("validate","validated");
+
+                    //reg to Db, upload data to db
+                    String user_email=userEmail.getText().toString().trim();
+                    String user_password=userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email,user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            Log.d("complete","completed");
+                            if(task.isSuccessful()){
+                                Log.d("task","task done");
+                               //sendEmailVerification();
+                                sendUserData();
+                                Toast.makeText(RegistrationActivity.this,"Successfully Registered, Upload Complete",Toast.LENGTH_SHORT).show();
+
+                                finish();
+                                startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+
+                            }else{
+                                Exception e = task.getException();
+                                Log.d("Exception:"+ e.toString(),"Exception"+e.toString());
+
+                                Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+
+
+                }
+
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+            }
+        });
+
+
+    }
+
+    private void setupUIViews(){
+        userName = (EditText)findViewById(R.id.etUserName);
+        userPassword=(EditText)findViewById(R.id.etUserPassword);
+        userEmail=(EditText)findViewById(R.id.etUserEmail);
+        regButton=(Button)findViewById(R.id.btnRegister);
+        userLogin=(TextView)findViewById(R.id.tvuserlogin);
+        userAge=(EditText)findViewById(R.id.etAge);
+        //userProfilePic=(ImageView)findViewById(R.id.ivProfile);
+    }
+
+    private Boolean validate(){
+        Boolean result=false;
+
+         name= userName.getText().toString();
+         password = userPassword.getText().toString();
+         email = userEmail.getText().toString();
+         age=userAge.getText().toString();
+
+
+        if(name.isEmpty() || password.isEmpty() || email.isEmpty() || age.isEmpty()){
+            Toast.makeText(this,"Please enter all the details",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            result=true;
+        }
+        return result;
+    }
+
+    private void sendEmailVerification(){
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null){
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        sendUserData();
+                        Toast.makeText(RegistrationActivity.this,"Successfully Registered, Verification mail has been sent",Toast.LENGTH_SHORT).show();
+                        firebaseAuth.signOut();
+                        finish();
+                        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(RegistrationActivity.this,"Verification mail hasn't been sent",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void sendUserData(){
+        FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
+        DatabaseReference myRef= firebaseDatabase.getReference(firebaseAuth.getUid());
+        UserProfile userProfile = new UserProfile(age,email,name);
+        myRef.setValue(userProfile);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/logindemo/UserProfile.java
@@ -0,0 +1,13 @@
+package com.example.logindemo;
+
+public class UserProfile {
+    public String Age;
+    public String Email;
+    public String Name;
+
+    public UserProfile(String Age, String Email, String Name) {
+        this.Age = Age;
+        this.Email = Email;
+        this.Name = Name;
+    }
+}
